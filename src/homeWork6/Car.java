5578739059a38507d7b19598f6f326f569dd19ac
Shkurenko.A <chilipili997@gmail.com>
2023-03-06 12:44:23 +0200

    solved hw6

---
+++ src/homeWork6/Car.java
@@ -0,0 +1,22 @@
+package homeWork6;
+
+public class Car {
+
+    private void startElectricity(){
+        System.out.println("start electricity");
+    }
+
+    private void startCommand(){
+        System.out.println("start command");
+    }
+
+    private void startFuelSystem(){
+        System.out.println("start fuel system");
+    }
+
+    public void start(){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+}
