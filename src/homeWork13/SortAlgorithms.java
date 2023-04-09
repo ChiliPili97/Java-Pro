a51807696e4c409fe4a063a1227bd05e0efe9144
Shkurenko.A <chilipili997@gmail.com>
2023-04-09 17:51:48 +0300

    fixed remark

--- src/homeWork13/SortAlgorithms.java
+++ src/homeWork13/SortAlgorithms.java
@@ -3,14 +3,14 @@ package homeWork13;
 public class SortAlgorithms {
 
     public static int[] insertionSort(int[] array) {
-        for (int i = 1; i < array.length; i++) {
-            int j = i;
-            while (j > 0 && array[j] < array[j - 1]) {
-                int temp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = temp;
-                j--;
+        for (int j = 1; j < array.length; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while ((i >= 0) && (array[i] > key)) {
+                array[i + 1] = array[i];
+                i--;
             }
+            array[i + 1] = key;
         }
         return array;
     }
