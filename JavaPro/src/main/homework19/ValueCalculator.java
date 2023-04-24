0f16fce04f60f20bc2bd8271ffc80729b7f8880f
Shkurenko.A <chilipili997@gmail.com>
2023-04-24 17:03:17 +0300

    solved homework19

--- JavaPro/src/main/homework19/ValueCalculator.java
+++ JavaPro/src/main/homework19/ValueCalculator.java
@@ -1,33 +1,67 @@
 package homework19;
 
 public class ValueCalculator {
-    int[] arr = new int[1_000_000];
-    private int size = arr.length;
-    private int halfSize = arr.length / 2;
+    private float[] arr;
+    private int size;
+    private int halfSize;
+    private long elapsedTime;
 
-    public int getSize() {
-        return size;
+    public long getElapsedTime() {
+        return elapsedTime;
     }
 
-    public int getHalfSize() {
-        return halfSize;
+    public ValueCalculator(int size) {
+        if (size < 1_000_000) {
+            size = 1_000_000;
+        }
+        this.size = size;
+        this.halfSize = size / 2;
+        this.arr = new float[size];
     }
 
-    public void fillArray() {
+    public void fillArrays() {
         long start = System.currentTimeMillis();
-        int[] a1 = new int[halfSize];
-        int[] a2 = new int[halfSize];;
-        for (int i = 0; i < getSize(); i++){
+        float[] a1 = new float[arr.length / 2];
+        float[] a2 = new float[arr.length / 2];
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = 1;
         }
+        System.arraycopy(arr, 0, a1, 0, halfSize);
+        System.arraycopy(arr, halfSize, a2, 0, halfSize);
 
-        //System.arraycopy(arr, 0, a1, halfSize);
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a1.length; i++) {
+                    a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
 
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a2.length; i++) {
+                    a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
 
+        t1.start();
+        t2.start();
 
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
+        System.arraycopy(a1, 0, arr, 0, halfSize);
+        System.arraycopy(a2, 0, arr, halfSize, halfSize);
 
         long end = System.currentTimeMillis();
-        System.out.println(end - start);
+        elapsedTime = end - start;
     }
 }
--- JavaPro/src/main/homework19/ValueCalculatorDemo.java
+++ JavaPro/src/main/homework19/ValueCalculatorDemo.java
@@ -2,7 +2,8 @@ package homework19;
 
 public class ValueCalculatorDemo {
     public static void main(String[] args) {
-        ValueCalculator valueCalculator = new ValueCalculator();
-        valueCalculator.fillArray();
+        ValueCalculator valueCalculator = new ValueCalculator(3_000_000);
+        valueCalculator.fillArrays();
+        System.out.println(valueCalculator.getElapsedTime());
     }
 }
