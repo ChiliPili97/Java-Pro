58a574d9ce26ab999cbcfb8571532015f0f48994
Andrii Shkurenko <65247103+ChiliPili97@users.noreply.github.com>
2023-07-17 14:07:49 +0300

    fixed remarks

--- src/main/java/coffee/order/CoffeeOrderBoard.java
+++ src/main/java/coffee/order/CoffeeOrderBoard.java
@@ -1,40 +1,17 @@
 package coffee.order;
 
-import java.io.IOException;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.*;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+
 
 public class CoffeeOrderBoard {
-    private Map<Long, Order> orders = new HashMap<Long, Order>();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CoffeeOrderBoard.class);//Logger.getLogger(CoffeeOrderBoard.class.getName());
     private long numberOfOrders;
-    private static final Logger LOGGER = Logger.getLogger(CoffeeOrderBoard.class.getName());
-    private static final String DEFAULT_PATH_FOR_LOG = "D:/Log.txt";
-    private String pathForLog;
-    private FileHandler fh;
-
-    public CoffeeOrderBoard() {
-        setPathForLog(DEFAULT_PATH_FOR_LOG);
-    }
-
-    public String getPathForLog() {
-        return pathForLog;
-    }
-
-    public void setPathForLog(String pathForLog) {
-        this.pathForLog = pathForLog;
-        try {
-            fh = new FileHandler(pathForLog);
-            LOGGER.addHandler(fh);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fh.setFormatter(formatter);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Rise new run time exception", e);
-            throw new RuntimeException(e);
-        }
-    }
+    private Map<Long, Order> orders = new HashMap<Long, Order>();
 
     public void add(Order order) {
         Objects.requireNonNull(order);
